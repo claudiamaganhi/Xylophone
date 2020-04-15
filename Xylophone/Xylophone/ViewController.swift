b2ea58db952f172e12f47cdd678f6d1682d29bb9
Claudia Maganhi <claudia.maganhi@gmail.com>
2020-04-15 16:54:14 -0300

    Create xylophone behaviour

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setButtonStyle()
     }
-
+    
+    @IBAction func playNote(_ sender: UIButton) {
+        playSound(forResource: sender.titleLabel?.text ?? "")
+    }
+    
+    private func setButtonStyle() {
+        buttonCollection.forEach { $0.layer.cornerRadius = 30 }
+        buttonCollection.forEach { $0.layer.masksToBounds = true }
+    }
+    
+    private func playSound(forResource: String) {
+        guard let url = Bundle.main.url(forResource: forResource, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            player.prepareToPlay()
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
